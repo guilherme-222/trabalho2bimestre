bcbf1b29a31b8f6a0fe2d34f43c17ba41fd9d5c0
guilherme-222 <jesus.g@escolar.ifrn.edu.br>
2025-08-06 16:05:53 -0300

    3.4

---
+++ interface2bimestre/Assets/Settings/scripts/CarregarCena.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregarCena : MonoBehaviour
+{
+    public void Carregar()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    
+    }
+
+
